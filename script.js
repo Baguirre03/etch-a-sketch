2cfd54574bf7a864ba34abb9fd50030ae9823981
Ben Aguirre <ben.aguirre03@gmail.com>
2022-11-03 15:44:54 -0500

    Grid size updates: 	- Can now update grid size with each different input 	- Clears original input and puts new one depending on type message 	- Changed reset button to "clear"

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 var input = document.querySelector('#amountGrid')
 var enter = document.querySelector('.enter')
+let pad = document.querySelector('.pad')
 
 enter.addEventListener('click', (e) => {
     if (input.value > 100) {
         alert("Number is too high! Nothing above 100")
     } else {
-    createBoard(input.value)
+        removeSketch();
+        createBoard(input.value)
     }
 })
 
 function createBoard(size) {
 
-    let pad = document.querySelector('.pad')
         pad.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
         pad.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
     for (let i = 0; i <= `${size * size}`; i++) {
         let box = document.createElement('div');
-        box.style = "border-color: black; border-style: dashed; border-width: .5px"
+        // box.style = "border-color: black; border-style: dashed; border-width: .5px"
         pad.appendChild(box)
         box.classList.add('boxes')
         box.style.color = "white"
@@ -32,5 +33,14 @@ function createBoard(size) {
     }
 }
 
-console.log(input);
+window.onload = function () {
+    createBoard(16);
+}
+
+function removeSketch () {
+    while (pad.firstChild) {
+        pad.removeChild(pad.firstChild);
+    }
+}
+
 
