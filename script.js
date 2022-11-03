97a4aa164dbda460348f8d0752ef51f5c2057e72
Ben Aguirre <ben.aguirre03@gmail.com>
2022-11-02 21:47:41 -0500

    Styling, JS updates 	-Updates JS, now has an input for grid that can only be lower than 100 	-Styling updates and buttons added 	-Background color changed

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-const container = document.querySelector('.container')
+var input = document.querySelector('#amountGrid')
+var enter = document.querySelector('.enter')
 
-for (let i = 1; i <= 256; i++) {
-    var div = document.createElement('div');
-    div.id = 'r'+i;
-    div.style = "border-color: green; border: solid;"
-    div.textContent = "ts"
-    container.appendChild(div)
+enter.addEventListener('click', (e) => {
+    if (input.value > 100) {
+        alert("Number is too high! Nothing above 100")
+    } else {
+    createBoard(input.value)
+    }
+})
+
+function createBoard(size) {
+
+    let pad = document.querySelector('.pad')
+        pad.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+        pad.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+    for (let i = 0; i <= `${size * size}`; i++) {
+        let box = document.createElement('div');
+        box.style = "border-color: black; border-style: dashed; border-width: .5px"
+        pad.appendChild(box)
+        box.classList.add('boxes')
+        box.style.color = "white"
+        box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = "orange"
+        })
+
+    let resetButton = document.querySelector('#resetBtn')
+        resetButton.addEventListener('click', () => {
+        box.style.backgroundColor = "white"
+        }) 
+    }
 }
+
+console.log(input);
+
